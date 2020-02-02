59fe28740a0106e97468bd0e16915a1208e0ad6d
SterlingVu <sterling.vu.goldenowl@gmail.com>
2020-02-02 23:30:37 +0700

    add js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,16 +18,27 @@
 //= require bootstrap-sprockets
 
 //= require_tree .
-var btnContainer = document.getElementById("edit-left-menu");
 
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("edit-tab");
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+$(document).ready(function(){
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 80) {
+      $("div.header-line").style.display = 'none';
+      $("div.ins-logo")..style.display = 'none';
+      $('header').addClass('scroll-header');
+    } else {
+      $("div.header-line")..style.display = 'inline-block';
+      $("div.ins-logo")..style.display = 'inline-block';
+      $('header').removeClass('scroll-header');
+    }
   });
-}
+
+  var btnContainer = document.getElementById("edit-left-menu");
+  var btns = btnContainer.getElementsByClassName("edit-tab");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+});
